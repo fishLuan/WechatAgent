335f70e85c16889a14feaf310e14e45fda190507
whirlwind123 <3420058857@qq.com>
2026-07-15 18:00:51 +0800

    初始化SpringBoot项目

---
+++ demo/src/test/java/com/xwc/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xwc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
